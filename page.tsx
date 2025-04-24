d872998268ce222316101471aa27ae8902635985
donggonlee <bm334@naver.com>
2025-04-24 16:15:56 +0900

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "../components/ui/tabs";
+
+// 이하 생략 - 실제 코드는 canvas 문서에서 가져와야 함
